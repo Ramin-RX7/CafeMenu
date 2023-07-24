529569abba163bcb7c8c51c73e794b39a9d5c771
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-24 13:47:01 +0330

    feat: add Order model in orders models file

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from foods.models import Food
 
-# Create your models here.
+class Order(models.Model):
+    items = models.IntegerFields()
+    price = models.TextFields()
+    discount = models.FloatFields(blank=True, null=True)
+    date = models.DateTimeFields(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=CASCADE)
+
+    def __str__(self) -> str:
+        return self.items
