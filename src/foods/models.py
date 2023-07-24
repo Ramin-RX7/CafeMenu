c96e5d3cd18180341439bd1c103e8f4109ad98fb
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-24 13:45:12 +0330

    feat: add Food model in foods models file

--- src/foods/models.py
+++ src/foods/models.py
@@ -6,3 +6,16 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+class Food(models.Model):
+    title = models.CharFields(max_length=50)
+    description = models.TextFields(blank=True, null=True)
+    price = models.FloatFields()
+    quantity = models.IntegerField()
+    discount = models.FloatFields(blank=True, null=True)
+    # I set null=True because st we dont have descount
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    # I use protect because we can't delete this table easy and its importent table for our site
+
+    def __str__(self) -> str:
+        return self.title
