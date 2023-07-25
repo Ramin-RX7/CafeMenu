5d3228a2e69b7356099d0d2498a4ed29f20ef88c
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-25 21:45:27 +0330

    dir for template fixed in setting

--- src/core/settings.py
+++ src/core/settings.py
@@ -54,8 +54,8 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        # 'DIRS': [ BASE_DIR/"templates" ],
-        "APP_DIRS": True,
+        'DIRS': [ BASE_DIR/"templates" ],
+        # "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
