19cb59b2e5c0f095a088a7180d13ba1772193d2c
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-26 14:20:19 +0330

    #28 define two function to render the template in view.py

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Order,OrderItem
 # Create your views here.
 
+# def order_list(request):
+#     return HttpResponse("hello world")
 def order_list(request):
-    return HttpResponse("hello world")
+    all_orders = Order.objects.all()
+    return render(request,'Order/order_list.html',{"all_orders":all_orders})
 
-def order_details(request):
-    return HttpResponse("hello world")
+# def order_details(request,pk):
+#     return HttpResponse("hello world")
+
+def order_details(request,pk):
+    order = OrderItem.objects.get(id=pk)
+    return render(request,'Order/order_details.html',{"order": order})
