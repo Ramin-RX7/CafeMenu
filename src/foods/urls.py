a227b0bd03b2d162d701d92b53d58253e95fdbbc
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-26 11:26:27 +0330

    feat: add search url in foods urls file

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import category_list, food_details
+from .views import category_list, food_details , search
 
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
     path("food_details/", food_details, name="food_details"),
+    path("search/", search, name="search"),
 ]
