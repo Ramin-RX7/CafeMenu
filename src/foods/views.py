d021cc786da9040e913cd944bd2e8c8cff2dacf1
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-26 14:26:29 +0330

    category_list function add to view.py in foods

--- src/foods/views.py
+++ src/foods/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Category,Food
 # Create your views here.
 
-def category_list(request):
-    return HttpResponse("hello world")
+# def category_list(request):
+#     return HttpResponse("hello world")
 
-def food_details(request):
-    return HttpResponse("hello world")
+def category_list(request):
+    all_category = Category.objects.all()
+    return render(request,'foods/category_list.html',{"all_category":all_category})
+
+# def food_details(request):
+#     return HttpResponse("hello world")
