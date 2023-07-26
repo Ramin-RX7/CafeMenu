56b0a1e6364a76383d6ed4a245953d3b9866fe32
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-26 14:32:28 +0330

    foods urls file modifiyed

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -3,5 +3,5 @@ from .views import category_list, food_details
 
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
-    path("food_details/", food_details, name="food_details"),
+    path("food_details/<int:pk>/", food_details, name="food_details"),
 ]
