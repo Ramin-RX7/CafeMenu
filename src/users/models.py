3e40af449720fd9560bf938478e1a74a7d4440f9
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-28 12:42:20 +0330

    feat: add User model

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import RegexValidator
 
 
 
@@ -13,4 +14,21 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    phone_validator = RegexValidator(r"(((+|00)(98))|0)9\d{2}-?\d{3}-?\d{4}")
+    phone = models.CharField(validators=[phone_validator], unique=True, max_length=20)
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.phone}"
