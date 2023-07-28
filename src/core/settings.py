d0624195d285f244722f5ed3f6905ffbb0faf2c7
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-28 12:48:58 +0330

    chore: set User as auth_user_model in settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -129,3 +129,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
