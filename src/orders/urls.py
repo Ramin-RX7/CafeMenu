b931dbe0b0145dab30701d1c13121c447ad623ff
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-28 20:51:17 +0330

    primerry key add to urls order details

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -3,5 +3,5 @@ from .views import order_list, order_details
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
-    path("order_details/", order_details, name="order_details"),
+    path("order_details/<int:pk>", order_details, name="order_details"),
 ]
