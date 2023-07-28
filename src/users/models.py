a82bf99aa3ac0ea1237682e24c8c834d7f9c6b68
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-28 12:23:47 +0330

    add UserManager create_user method

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password, **other_fields):
+        if phone is None:
+            raise ValueError("Phone not given")
+
+        user = self.model(phone=phone, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 # Create your models here.
