47db125f739460613df684402c5a8084b81640b1
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-28 21:36:00 +0330

    chore: add UserAuthBackend in the settings auth backends

--- src/core/settings.py
+++ src/core/settings.py
@@ -131,3 +131,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+AUTHENTICATION_BACKENDS = [
+    'users.auth.UserAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
