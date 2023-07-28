9e2d870a599d9767b3f47939d6ca1c32e5e37303
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-28 20:46:02 +0330

    functiion order_list add to view in order app

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Order,OrderItem
 # Create your views here.
 
 def order_list(request):
-    return HttpResponse("hello world")
+    all_orders = Order.objects.all()
+    return render(request,'Order/order_list.html',{"all_orders":all_orders})
 
 def order_details(request):
     return HttpResponse("hello world")
