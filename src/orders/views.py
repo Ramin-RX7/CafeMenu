0f5dbc0f0df026c551104b80e85d654efc112d5a
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-28 20:49:19 +0330

    function order_details add to view in order app

--- src/orders/views.py
+++ src/orders/views.py
@@ -7,5 +7,6 @@ def order_list(request):
     all_orders = Order.objects.all()
     return render(request,'Order/order_list.html',{"all_orders":all_orders})
 
-def order_details(request):
-    return HttpResponse("hello world")
+def order_details(request,pk):
+    order = OrderItem.objects.get(id=pk)
+    return render(request,'Order/order_details.html',{"order": order})
