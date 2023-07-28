463880f517aa03e2e7c88357144d650080ac689f
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-28 21:00:04 +0330

    context name changed in order_list function in view

--- src/orders/views.py
+++ src/orders/views.py
@@ -4,8 +4,8 @@ from .models import Order,OrderItem
 # Create your views here.
 
 def order_list(request):
-    all_orders = Order.objects.all()
-    return render(request,'Order/order_list.html',{"all_orders":all_orders})
+    plural  = Order.objects.all()
+    return render(request,'Order/order_list.html',{"plural": plural })
 
 def order_details(request,pk):
     order = OrderItem.objects.get(id=pk)
