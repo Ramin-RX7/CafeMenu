491444255715e53fe65f895cf267433181f4b2ab
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-29 11:38:33 +0330

    context name fixed

--- src/orders/views.py
+++ src/orders/views.py
@@ -4,9 +4,9 @@ from .models import Order,OrderItem
 # Create your views here.
 
 def order_list(request):
-    plural  = Order.objects.all()
-    return render(request,'Order/order_list.html',{"plural": plural })
+    orders  = Order.objects.all()
+    return render(request,'Order/order_list.html',{"orders": orders })
 
 def order_details(request,pk):
-    order = OrderItem.objects.get(id=pk)
+    order = Order.objects.get(id=pk)
     return render(request,'Order/order_details.html',{"order": order})
