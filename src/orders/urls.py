273c3a824dc4600e97db9ef5228d1fec7544ca70
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-30 00:09:49 +0330

    pk changed to id in urls in orders app

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -3,6 +3,6 @@ from .views import order_list, order_details,cart
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
-    path("order_details/<int:pk>", order_details, name="order_details"),
+    path("order_details/<int:id>", order_details, name="order_details"),
     path("cart/",cart, name="cart"),
 ]
