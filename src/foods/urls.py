1d2658dc52e5096c48c045f9093fcab5f396dd08
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-30 00:02:53 +0330

    changed pk to id in urls in foods app

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -3,7 +3,7 @@ from .views import category_list, food_details , search , category_details
 
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
-    path("categories/<int:pk>/", category_details, name="category_details"),
-    path("food_details/<int:pk>/", food_details, name="food_details"),
+    path("categories/<int:id>/", category_details, name="category_details"),
+    path("food_details/<int:id>/", food_details, name="food_details"),
     path("search/", search, name="search"),
 ]
