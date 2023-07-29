06bb127beb07238d4e2f936dcefd230924ade15c
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-29 11:56:43 +0330

    category details urls add in urls file in foods app

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import category_list, food_details , search
+from .views import category_list, food_details , search , category_details
 
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
+    path("categories/<int:pk>/", category_details, name="category_details"),
     path("food_details/<int:pk>/", food_details, name="food_details"),
     path("search/", search, name="search"),
 ]
