9360328fae221ccbd137f2880de027ef5815767a
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-29 14:50:07 +0330

    fix: set templates lookup to APP_DIR to True

--- src/core/settings.py
+++ src/core/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         'DIRS': [ BASE_DIR/"templates" ],
-        # "APP_DIRS": True,
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
