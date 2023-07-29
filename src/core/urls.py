2f55d2c8eb53010311791bf992a8800649cd1bfd
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-29 15:34:44 +0330

    fix: add media files url to main urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("foods/", include("foods.urls")),
     path("orders/", include("orders.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
