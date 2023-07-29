9e8239b19622a1cb51ff477be677d298667fc62f
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-29 12:12:00 +0330

    function cart added to the views in orders app

--- src/orders/views.py
+++ src/orders/views.py
@@ -5,8 +5,12 @@ from .models import Order,OrderItem
 
 def order_list(request):
     orders  = Order.objects.all()
-    return render(request,'Order/order_list.html',{"orders": orders })
+    return render(request,'CafeMenu/src/templates/orders/order_list.html',{"orders": orders })
 
 def order_details(request,pk):
     order = Order.objects.get(id=pk)
-    return render(request,'Order/order_details.html',{"order": order})
+    return render(request,'CafeMenu/src/templates/orders/order_details.html',{"order": order})
+
+def cart(request):
+    context = None
+    return render(request,'CafeMenu/src/templates/orders/cart.html',{"context": context})
