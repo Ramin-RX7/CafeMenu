295dfa766bb2737e8d8c155adef3ea7caea684b3
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-30 00:04:39 +0330

    templats name fixed in views foods app

--- src/foods/views.py
+++ src/foods/views.py
@@ -6,17 +6,20 @@ from .models import Category,Food
 
 def category_list(request):
     categories = Category.objects.all()
-    return render(request,'CafeMenu/src/templates/foods/category_list.html',{"categories":categories})
+    context = {"categories":categories}
+    return render(request,'foods/category_list.html',context)
 
-def category_details(request,pk):
-    category = Category.objects.get(id=pk)
-    return render(request,'CafeMenu/src/templates/foods/category_details.html',{"category":category})
+def category_details(request,id):
+    category = Category.objects.get(id=id)
+    context = {"category":category}
+    return render(request,'foods/category_details.html',context)
 
-def food_details(request, pk):
-    food = Food.objects.get(id=pk)
-    return render(request, "CafeMenu/src/templates/foods/food_details.html", {"food": food})
+def food_details(request, id):
+    food = Food.objects.get(id=id)
+    context = {"food": food}
+    return render(request, "foods/food_details.html",context)
 
 def search(request):
     if request.method != "GET":
         raise Http404
-    return render(request,'CafeMenu/src/templates/foods/search.html')
+    return render(request,'foods/search.html')
