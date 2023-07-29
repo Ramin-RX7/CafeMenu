1ea73cb09e3b8253e14dbf896500ae8d2dc51a4d
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-30 00:10:27 +0330

    templats name changed in views orders app

--- src/orders/views.py
+++ src/orders/views.py
@@ -5,12 +5,15 @@ from .models import Order,OrderItem
 
 def order_list(request):
     orders  = Order.objects.all()
-    return render(request,'CafeMenu/src/templates/orders/order_list.html',{"orders": orders })
+    context = {"orders": orders }
+    return render(request,'orders/order_list.html',context)
 
-def order_details(request,pk):
-    order = Order.objects.get(id=pk)
-    return render(request,'CafeMenu/src/templates/orders/order_details.html',{"order": order})
+def order_details(request,id):
+    order = Order.objects.get(id=id)
+    context = {"order": order}
+    return render(request,'orders/order_details.html',context)
 
 def cart(request):
-    context = None
-    return render(request,'CafeMenu/src/templates/orders/cart.html',{"context": context})
+    c_rt = None
+    context = {"c_rt": c_rt}
+    return render(request,'orders/cart.html',context)
