73f978665dc0feb58aaf98b62a3852bbe4101f23
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-29 11:45:22 +0330

    templates urls fixed in views

--- src/foods/views.py
+++ src/foods/views.py
@@ -6,13 +6,13 @@ from .models import Category,Food
 
 def category_list(request):
     plural = Category.objects.all()
-    return render(request,'foods/category_list.html',{"plural":plural})
+    return render(request,'CafeMenu/src/templates/foods/category_list.html',{"plural":plural})
 
 def food_details(request, pk):
     food = Food.objects.get(id=pk)
-    return render(request, "food/food_details.html", {"food": food})
+    return render(request, "CafeMenu/src/templates/foods/food_details.html", {"food": food})
 
 def search(request):
     if request.method != "GET":
         raise Http404
-    return render(request,'foods/search.html')
+    return render(request,'CafeMenu/src/templates/foods/search.html')
