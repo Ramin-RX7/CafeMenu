07a2d6a786e3c37cbca6b5fa3603cdb5afe263e4
sajjadp96 <sajjad.piri1997@gmail.com>
2023-07-29 12:13:02 +0330

    ulrs set for cart function in urls orders app

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import order_list, order_details
+from .views import order_list, order_details,cart
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
     path("order_details/<int:pk>", order_details, name="order_details"),
+    path("cart/",cart, name="cart"),
 ]
