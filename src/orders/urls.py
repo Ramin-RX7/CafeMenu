aa57f37edea73cca226f81de8eee6e998b5da890
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-30 17:02:50 +0330

    add app_name to orders/urls.py

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import order_list, order_details,cart
+from .views import *
+
+
+app_name = "orders"
+
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
