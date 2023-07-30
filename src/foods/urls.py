b38debbfd1e63cb7f50de958bf70c63bcaa6bdb3
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-30 17:10:34 +0330

    chore: update food details and category details url

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -8,8 +8,8 @@ app_name = "foods"
 
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
-    path("categories/<int:id>/", category_details, name="category_details"),
-    path("food_details/<int:id>/", food_details, name="food_details"),
+    path("category/<int:id>/", category_details, name="category_details"),
+    path("food/<int:id>/", food_details, name="food_details"),
     path("search/", search, name="search"),
     path("", menu, name="menu")
 ]
