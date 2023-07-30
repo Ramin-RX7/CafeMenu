6affc32a684b2d8b81328338357cee898bafeaff
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-31 01:15:48 +0330

    chore: add comment for cart_add in urls

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -8,6 +8,7 @@ app_name = "orders"
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
     path("order/<int:id>/", order_details, name="order_details"),
-    path("cart/",cart, name="cart"),
-    path("", index, name="index")
+    path("cart/", cart, name="cart"),
+    # path("cart/add/<int:id>", cart_add, name="cart_add"),
+    path("", index, name="index"),
 ]
