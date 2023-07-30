45ec80624ea7b3f62251f2414ab4c9a94ca748c2
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-30 17:03:24 +0330

    refactor: implement index page and order_list redirect

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("order_list/", order_list, name="order_list"),
     path("order_details/<int:id>/", order_details, name="order_details"),
     path("cart/",cart, name="cart"),
+    path("", index, name="index")
 ]
