85b070610b40d70daa1241f91106ac9f984e07c1
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-30 18:12:55 +0330

    refactor: update food_details url

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -9,7 +9,7 @@ app_name = "foods"
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
     path("category/<int:id>/", category_details, name="category_details"),
-    path("food/<int:id>/", food_details, name="food_details"),
     path("search/", search, name="search"),
+    path("<int:id>/", food_details, name="food_details"),
     path("", menu, name="menu")
 ]
