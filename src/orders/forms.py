73a28c55c267e503fa68dd13482e28b694756f3d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-31 02:33:16 +0330

    chor: add image field in forms

--- src/orders/forms.py
+++ src/orders/forms.py
@@ -6,3 +6,5 @@ from users.models import PhoneNumberField
 phone_validator = RegexValidator(r"(((\+|00)(98))|0)?9\d{2}-?\d{3}-?\d{4}")
 class CustomerLoginForm(forms.Form):
     phone = PhoneNumberField(validators=[phone_validator], unique=True, max_length=20)
+    image=forms.ImageField(upload_to='static/',blank=True,null=True)
+
