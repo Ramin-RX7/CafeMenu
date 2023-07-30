7237182507944ffb6137349cd42965b78f03254e
Ramin RX7 <rawmin.rx@gmail.com>
2023-07-30 17:09:18 +0330

    chore: update order details url

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -7,7 +7,7 @@ app_name = "orders"
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
-    path("order_details/<int:id>/", order_details, name="order_details"),
+    path("order/<int:id>/", order_details, name="order_details"),
     path("cart/",cart, name="cart"),
     path("", index, name="index")
 ]
