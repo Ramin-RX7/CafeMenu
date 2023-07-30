7063f78c26c172ae8c8bb195a8355c73536a3cc3
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-30 15:56:47 +0330

    chor: add url path for customer login

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import order_list, order_details
+from .views import order_list, order_details, customer_login
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
     path("order_details/", order_details, name="order_details"),
+    path("customer_login/", customer_login,name="customer_login"),
 ]
