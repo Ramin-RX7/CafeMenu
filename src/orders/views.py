c61e082526f9f270211aa7a67ceddc0f0ff9030c
hosseink9 <hosseinkalantari.29@gmail.com>
2023-07-30 15:55:07 +0330

    chor: add customer_login in orders views

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import CustomerLoginForm
 # Create your views here.
 
 def order_list(request):
@@ -7,3 +8,11 @@ def order_list(request):
 
 def order_details(request):
     return HttpResponse("hello world")
+
+def customer_login(request):
+    if request.method == "POST":
+        form = CustomerLoginForm(request.POST)
+        if form.is_valid():
+            phone=form.cleaned_data['phone']
+            request.session['phone']=phone
+    return redirect('menu.html')
