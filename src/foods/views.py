29c39521048888f9d18f39e47db708ea0dbbd52f
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-07-31 13:47:52 -0700

    added view for menu

--- src/foods/views.py
+++ src/foods/views.py
@@ -28,4 +28,7 @@ def search(request):
 
 
 def menu(request):
-    return render(request, "foods/menu.html")
+    categories = Category.objects.prefetch_related('food_set').all()
+    context = {"categories":categories}
+    return render(request, "foods/menu.html", context)
+
