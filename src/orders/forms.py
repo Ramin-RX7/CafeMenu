b1f3c379740c78be6939323d8f88dbea7c988cc1
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-01 15:46:59 +0330

    feat: add phone input to customer_login form

--- src/orders/forms.py
+++ src/orders/forms.py
@@ -4,5 +4,13 @@ from users.models import PhoneNumberField
 
 
 phone_validator = RegexValidator(r"(((\+|00)(98))|0)?9\d{2}-?\d{3}-?\d{4}")
+
+
+class PhoneInput(forms.widgets.TextInput):
+    def __init__(self, *args, **kwargs):
+        attrs = kwargs.setdefault('attrs', {})
+        attrs['class'] = 'form-control'
+        super().__init__(*args, **kwargs)
+
 class CustomerLoginForm(forms.Form):
-    phone = PhoneNumberField(validators=[phone_validator], unique=True, max_length=20)
+    phone = forms.CharField(validators=[phone_validator], max_length=20, widget=PhoneInput)
