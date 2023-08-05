ff0a7323d08a2b4df01819a5265d25950f91b004
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-04 18:02:21 -0700

    chore: user login view added

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserLogInForm
+from auth import UserAuthBackend
+from django import http
 
-# Create your views here.
+def login(request):
+    if request.method=="POST":
+        form=UserLogInForm(request.POST)
+        if form.is_valid():
+            cd=form.changed_data
+            phone=cd['phone']
+            user=UserAuthBackend.authenticate(request, phone)
+            if user:
+                request.session['user_phone']=phone
+                return redirect('verify')
+        else:
+            return http.HttpResponse('invalid phone number')
+    else:
+        form=UserLogInForm()
+        context={'form':form}
+        return render(request, 'Users/login.html', context)
