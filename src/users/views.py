8b23512deb923aa4c690287a898fc04b5372b847
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-05 12:16:35 -0700

    feat: move views from user to panel

--- src/users/views.py
+++ src/users/views.py
@@ -1,25 +1,5 @@
-from django.shortcuts import render, redirect
-from .forms import UserLogInForm
-from .auth import UserAuthBackend
-from django.http import HttpResponse
+# Create your views here.
 
-def login(request):
-    if request.method=="POST":
-        form=UserLogInForm(request.POST)
-        if form.is_valid():
-            cd=form.changed_data
-            phone=cd['phone']
-            user=UserAuthBackend.authenticate(request, phone=phone)
-            if user:
-                request.session['user_phone']=phone
-                return redirect('verify')
-        else:
-            return HttpResponse('invalid phone number')
-    else:
-        form=UserLogInForm()
-        context={'form':form}
-        return render(request, 'users/login.html', context)
     
 
-def verify(request):
-    pass
+
