f43c8f04e37c8f6131bd3b1e4e2d9fdd1bdc1575
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-05 13:03:37 -0700

    chore: variable changed

--- src/panel/forms.py
+++ src/panel/forms.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator
 
 phone_validator = RegexValidator(r"(((\+|00)(98))|0)?9\d{2}-?\d{3}-?\d{4}")
 class UserLogInForm(forms.Form):
-    phone=forms.IntegerField(validators=[phone_validator])
+    phone=forms.CharField(validators=[phone_validator])
 
 class UserVerifyForm(forms.Form):
-    opt=forms.CharField(min_length=4 ,max_length = 4)
+    otp=forms.CharField(min_length=4 ,max_length = 4)
