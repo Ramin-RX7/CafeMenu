ab2992debae2b1bbbcedba5258661f0da42bc54b
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-05 17:36:38 +0330

    feat: add class Meta

--- src/main/models.py
+++ src/main/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class BaseModel(models.Model):
-    pass
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract=True 
