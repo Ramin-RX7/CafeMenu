b192afdcf357eecba7b18766b7f7a20dba98ef8d
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-05 13:43:53 -0700

    chore: user verify view debug

--- src/panel/views.py
+++ src/panel/views.py
@@ -22,24 +22,27 @@ def login(request):
     
 
 def user_verify(request):
-    form = UserVerifyForm()
     user_phone = request.session.get('user_phone')
-    generated_otp = random.randint(1000, 9999)
-    request.session['2FA'] = generated_otp
-    print(generated_otp)
-    
+    form = UserVerifyForm()
+
     if user_phone:
-        if request.method == 'POST':
+        if request.method == 'GET':
+            generated_otp = random.randint(1000, 9999)
+            request.session['2FA'] = generated_otp
+            print(generated_otp)
+        elif request.method == 'POST':
             form = UserVerifyForm(request.POST)
             if form.is_valid():
                 entered_otp = form.cleaned_data.get('otp')
                 generated_otp = request.session.get('2FA')
                 if entered_otp == str(generated_otp):
-                    request.session.pop('2FA') 
+                    request.session.pop('2FA')
                     return redirect("index")
-            else:
-                return redirect("panel:user_verify")
+                else:
+                    return redirect("panel:user_verify")
+
     return render(request, 'panel/user_verify.html', {'form': form})
+
 
 
 
