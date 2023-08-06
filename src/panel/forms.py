6d62d1086bfec13b06cec439506e7eb0062fefb4
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-07 01:43:05 +0330

    chore: add bootstrap styles to login form

--- src/panel/forms.py
+++ src/panel/forms.py
@@ -5,7 +5,13 @@ from django.core.validators import RegexValidator
 
 phone_validator = RegexValidator(r"(((\+|00)(98))|0)?9\d{2}-?\d{3}-?\d{4}")
 class UserLogInForm(forms.Form):
-    phone=forms.CharField(validators=[phone_validator])
+    phone=forms.CharField(
+        validators = [phone_validator],
+        widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': "Phone"
+        }))
+
 
 class UserVerifyForm(forms.Form):
-    otp=forms.CharField(min_length=4 ,max_length = 4)
+    otp=forms.CharField(min_length=4, max_length=4)
