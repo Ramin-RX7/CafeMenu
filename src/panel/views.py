f034afbe259e884686877fcae8a11c7f2a525122
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-06 17:07:06 +0330

    chore:complete  dashboard_staff view in panel app

--- src/panel/views.py
+++ src/panel/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from orders.models import Order, Table
 
 # Create your views here.
+
+def dashboard_staff(request):
+    orders = Order.objects.order_by('status')
+    tables = Table.objects.all()
+    context = {
+        'orders': orders ,
+        'tables': tables ,
+    }
+    return render(request,'panel/dashboard_staff.html', context)
