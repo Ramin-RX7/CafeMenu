15db2b52849695574394146707c285d679ed23e9
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-06 14:39:01 +0330

    chor: fix url category details

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -7,7 +7,7 @@ app_name = "orders"
 
 urlpatterns = [
     path("order_list/", order_list, name="order_list"),
-    path("order/<int:id>/", order_details, name="order_details"),
+    path("<int:id>/", order_details, name="order_details"),
     path("cart/", cart, name="cart"),
     path("", index, name="index"),
     path("cart/delete/",cart_delete,name="cart_delete"),
