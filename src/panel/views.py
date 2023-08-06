634f1c18a03c753ef1524387603f89e710880058
sajjadp96 <sajjad.piri1997@gmail.com>
2023-08-06 19:50:32 +0330

    panel:views.py func: dashboard price for each day of week defined

--- src/panel/views.py
+++ src/panel/views.py
@@ -15,20 +15,5 @@ def dashboard(request):
     price_for_each_day_of_month = all_orders.filter(created_at__gte=last_30_days).\
     extra({'day':"date(created_at)"}).\
     values('day').annotate(count=Sum('price'))
-    
-    price_for_each_day_of_week = all_orders.filter(created_at__gte=last_7_days).\
-    extra({'day':"date(created_at)"}).\
-    values('day').annotate(count=Sum('price'))
-    
-    #count of orders
-    
-    count_of_order_for_each_day_of_month = all_orders.filter(created_at__gte=last_30_days).\
-    extra({'day' : "date(created_at)"}).\
-    values('day').\
-    annotate(created_count=Count('id'))
-    
-    count_of_order_day_of_month = all_orders.filter(created_at__gte=last_7_days).\
-    extra({'day':"date(created_at)"}).\
-    values('day').annotate(created_count=Count('price'))
 
     
