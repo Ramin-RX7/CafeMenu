7e8c1cda3f34cd6b969e47414acaa498d39cfaa5
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-07 01:46:14 +0330

    feat: remake of user_verify with new features

--- src/panel/views.py
+++ src/panel/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
-from .forms import UserLogInForm, UserVerifyForm
-from django.http import HttpResponse
 import random
+from datetime import timedelta
+
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
 from users.models import User
+from .forms import UserLogInForm, UserVerifyForm
+
+
 
 def login(request):
     form=UserLogInForm()
@@ -33,29 +38,39 @@ def generate_2fa(request):
 
 def user_verify(request):
     user_phone = request.session.get('user_phone')
-    form = UserVerifyForm()
+    if not user_phone:
+        return redirect("panel:login")
+    generated_otp = request.session.get('2FA')
+    expiration_time = request.session.get('2fa_expire')
 
-    if user_phone:
-        if request.method == 'GET':
-            generated_otp = random.randint(1000, 9999)
-            request.session['2FA'] = generated_otp
-            print(generated_otp)
-        elif request.method == 'POST':
-            form = UserVerifyForm(request.POST)
+    if request.method == "GET":
+        form = UserVerifyForm()
+        if (not expiration_time) or (
+            timezone.now() > timezone.datetime.strptime(expiration_time, "%d/%m/%Y, %H:%M:%S")
+        ):
+            request = generate_2fa(request)
+        else:
+            # form.add_error("already generated")
+            print(f"previous:{generated_otp}  until:{expiration_time}")
+        return render(request, 'panel/user_verify.html', {'form': form})
+
+    elif request.method == "POST":
+        form = UserVerifyForm(request.POST)
+        if timezone.now() > timezone.datetime.strptime(expiration_time, "%d/%m/%Y, %H:%M:%S"):
+            print("expired")
+            request = generate_2fa(request)
+            form.add_error(None, "Previous 2FA code expired. A new code has been sent to you")
+            return render(request, 'panel/user_verify.html', {'form': form})
+        else:
             if form.is_valid():
                 entered_otp = form.cleaned_data.get('otp')
-                generated_otp = request.session.get('2FA')
                 if entered_otp == str(generated_otp):
                     request.session.pop('2FA')
+                    request.session.pop('2fa_expire')
+                    request.session["authenticated"] = True
                     return redirect("index")
                 else:
-                    return redirect("panel:user_verify")
-
-    return render(request, 'panel/user_verify.html', {'form': form})
-
-
-
-
-
-
-
+                    form.add_error(None,"Invalid code entered")
+                    return render(request, 'panel/user_verify.html', {'form': form})
+            else:
+                ...
