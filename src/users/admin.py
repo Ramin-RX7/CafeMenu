f5e0962d9f6f1dce823679892a02b0c49ea0a5ff
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-08 00:43:14 +0330

    chor: add filter for active or inactive user

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,8 +1,9 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
 from . import models
 from django.db.models.query import QuerySet
 
-class UserFilter(admin.SimpleListFilter):
+class UserStaffFilter(admin.SimpleListFilter):
     title = 'Staff'
     parameter_name = 'Staff'
 
@@ -14,7 +15,21 @@ class UserFilter(admin.SimpleListFilter):
             return queryset.filter(is_staff = False)
         if self.value() == 'is_staff':
             return queryset.filter(is_staff = True)
+        
+class UserActiveFilter(admin.SimpleListFilter):
+    title='Active/Inactive'
+    parameter_name = 'Active/Inactive'
+
+    def lookups(self, request, model_admin):
+        return[('active','active'),('inactive','inactive')]
+    
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == 'active':
+            return queryset.filter(is_active=True)
+        if self.value() == 'inactive':
+            return queryset.filter(is_active=False)
+        
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     search_fields = ['first_name','last_name','phone']
-    list_filter = [UserFilter]
+    list_filter = [UserStaffFilter,UserActiveFilter]
