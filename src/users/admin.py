b0bf09afd88a85122ce06dff4418528f97b79177
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-07 21:54:44 +0330

    chor: add search for User admin

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['first_name','last_name','phone']
+
