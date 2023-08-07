b15ddb6f725ef4b7b1baf8ef180da1f1e5fe63c8
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-07 18:04:03 +0330

    Feat : add order/edit/<int:order_id>/ to panel views.py

--- src/panel/urls.py
+++ src/panel/urls.py
@@ -6,5 +6,6 @@ app_name = "panel"
 
 
 urlpatterns = [
-    path("",dashboard_staff, name="dashboard_staff")
+    path("",dashboard_staff, name="dashboard_staff"),
+    path("order/edit/<int:order_id>/", edit_order, name="edit_order"),
 ]
