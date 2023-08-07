2b0ce1759cdec94bf2ad6eb71648e5a0c072610d
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-07 18:08:24 +0330

    Feat: add order/pay/<int:order_id>/ to panel views.py

--- src/panel/urls.py
+++ src/panel/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("order/edit/<int:order_id>/", edit_order, name="edit_order"),
     path("order/approve/<int:order_id>/", approve_order, name="approve_order"),
     path("order/reject/<int:order_id>/", reject_order, name="reject_order"),
+    path("order/pay/<int:order_id>/", pay_order, name="pay_order"),
 ]
