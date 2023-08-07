37e7183d366b2061ac9eec41433412b224bc1c0c
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-08 01:45:42 +0330

    feat: add food list to category in admin panel

--- src/foods/admin.py
+++ src/foods/admin.py
@@ -1,7 +1,11 @@
-from typing import Any, List, Optional, Tuple
 from django.contrib import admin
 from django.db.models.query import QuerySet
+from django.urls import reverse
+from django.utils.html import format_html
+
 from . import models
+
+
 
 class CategoryFilter(admin.SimpleListFilter):
     title = 'Empty Category'
@@ -9,17 +13,39 @@ class CategoryFilter(admin.SimpleListFilter):
 
     def lookups(self, request, model_admin):
         return[('title','Categories')]
-    
+
     def queryset(self, request, queryset: QuerySet):
         if self.value() == 'title':
             return queryset.filter(food = None)
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display=['title','created_at','updated_at']
+    list_display=['title','view_foods','created_at','updated_at']
     ordering = ['title']
     search_fields = ['title']
     list_filter = [CategoryFilter]
+
+
+    def view_foods(self, obj):
+        url = reverse(f'admin:{models.Food._meta.app_label}_{models.Food._meta.model_name}_changelist')
+        link = f'<a href="{url}?category__id__exact={obj.pk}">View Foods</a>'
+        return format_html(link)
+    view_foods.short_description = 'Foods'
+    readonly_fields = ('view_foods',)
+
+    def get_fieldsets(self, request, obj=None):
+        if obj is None:
+            return super().get_fieldsets(request, obj)
+        return  (
+            ('Category Details', {
+                'fields': ('title', 'description', 'image'),
+            }),
+            ('Associated Foods', {
+                'fields': ('view_foods',),
+            }),
+        )
+
+
 
 @admin.register(models.Food)
 class FoodAdmin(admin.ModelAdmin):
