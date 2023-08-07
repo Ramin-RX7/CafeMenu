e3fcb1bfeae1e340a7be02506794c85b92e0181c
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-07 18:05:03 +0330

    Feat : add order/approve/<int:order_id>/ to panel views.py

--- src/panel/urls.py
+++ src/panel/urls.py
@@ -8,4 +8,5 @@ app_name = "panel"
 urlpatterns = [
     path("",dashboard_staff, name="dashboard_staff"),
     path("order/edit/<int:order_id>/", edit_order, name="edit_order"),
+    path("order/approve/<int:order_id>/", approve_order, name="approve_order"),
 ]
