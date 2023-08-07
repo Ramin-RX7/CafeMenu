66377b3d557966b49fec2eeb5922e02f551a5d4e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-08 00:22:45 +0330

    chor: add category empty filter

--- src/foods/admin.py
+++ src/foods/admin.py
@@ -1,12 +1,25 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from . import models
 
-# Register your models here.
+class CategoryFilter(admin.SimpleListFilter):
+    title = 'Empty Category'
+    parameter_name = 'title'
+
+    def lookups(self, request, model_admin):
+        return[('title','Categories')]
+    
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == 'title':
+            return queryset.filter(food = None)
+
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display=['title','created_at','updated_at']
     ordering = ['title']
     search_fields = ['title']
+    list_filter = [CategoryFilter]
 
 @admin.register(models.Food)
 class FoodAdmin(admin.ModelAdmin):
