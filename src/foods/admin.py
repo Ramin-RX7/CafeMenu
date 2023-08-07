07d35e5a3be43f0e8a577b0d64271c3a667f8c46
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-07 18:12:18 +0330

    chor: add list display for category field

--- src/foods/admin.py
+++ src/foods/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
 from .models import *
+from . import models
 
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['title','description']
+
 admin.site.register(Food)
-admin.site.register(Category)
