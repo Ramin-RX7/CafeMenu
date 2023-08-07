a2adbcfc74b87106d97b4b0cd4bd748b9ef129c3
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-07 18:31:04 +0330

    chor: add list display for order item

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -6,5 +6,9 @@ from . import models
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display=['customer','table','status','price','discount']
-admin.site.register(OrderItem)
+
+@admin.register(models.OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display=['order','food','quantity','unit_price','discount']
+
 admin.site.register(Table)
