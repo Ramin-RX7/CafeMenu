7075c654c6ff7999246ae8a68ad87c879ea40efc
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-07 21:45:57 +0330

    fix: deleted order item in adminpanel

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
+from .models import *
 from . import models
 
-# Register your models here.
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display=['customer','table','status','price','discount','final_price']
-
-@admin.register(models.OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display=['order','food','quantity','unit_price','discount']
 
 
 @admin.register(models.Table)
