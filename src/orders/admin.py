a6c0daf8fb8e43b782c4740831a66b9e62c3a3c0
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-08 00:26:15 +0330

    chor: add ordering for order admin

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -6,7 +6,7 @@ from . import models
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display=['customer','table','status','price','discount','final_price']
-
+    ordering = ['created_at','updated_at']
 
 @admin.register(models.Table)
 class TableAdmin(admin.ModelAdmin):
