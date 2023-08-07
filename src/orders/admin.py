c3fa55a6ec9aad0adf775aa7ab59ad70a69f60b5
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-07 18:28:36 +0330

    chor: add list display for order

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-
 from .models import *
+from . import models
 
 # Register your models here.
-admin.site.register(Order)
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['customer','table','status','price','discount']
 admin.site.register(OrderItem)
 admin.site.register(Table)
