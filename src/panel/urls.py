5b604af8ed3dc63d358411f665f21f1d39561e5d
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-07 18:07:57 +0330

    Feat: add order/reject/<int:order_id>/ to panel views.py

--- src/panel/urls.py
+++ src/panel/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("",dashboard_staff, name="dashboard_staff"),
     path("order/edit/<int:order_id>/", edit_order, name="edit_order"),
     path("order/approve/<int:order_id>/", approve_order, name="approve_order"),
+    path("order/reject/<int:order_id>/", reject_order, name="reject_order"),
 ]
