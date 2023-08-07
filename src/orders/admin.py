c1f6759c8aac106bc69db4acea5d692c96813986
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-08 03:01:27 +0330

    chor: fix

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -11,20 +11,20 @@ class OrderToDayFilter(admin.SimpleListFilter):
 
     def lookups(self, request, model_admin):
         return[('today','today')]
-    
+
     def queryset(self, request, queryset: QuerySet):
         if self.value() == 'todaye':
             return queryset.filter(created_at = datetime.today())
-        
+
 class OrderStatusFilter(admin.SimpleListFilter):
     title = 'not paid'
     parameter_name = 'not paid'
 
     def lookups(self, request, model_admin):
         return[('not_paid','not_paid')]
-    
+
     def queryset(self, request, queryset: QuerySet):
-        if self.value() == 'not_paid':        
+        if self.value() == 'not_paid':
             return queryset.filter(~Q(status = 'Paid'))
 
 @admin.register(models.Order)
@@ -32,6 +32,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display=['customer','table','status','price','discount','final_price']
     ordering = ['created_at','updated_at']
     list_filter=[OrderToDayFilter,OrderStatusFilter]
+    list_per_page = 20
 
 @admin.register(models.Table)
 class TableAdmin(admin.ModelAdmin):
