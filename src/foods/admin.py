88e52232e9ce8b68d2dfb22774de46a27a1e02fc
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-07 19:54:45 +0330

    chore: add timestamps to `foods` app admin panel

--- src/foods/admin.py
+++ src/foods/admin.py
@@ -4,10 +4,10 @@ from . import models
 # Register your models here.
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display=['title','description']
+    list_display=['title','created_at','updated_at']
     ordering = ['title']
 
 @admin.register(models.Food)
 class FoodAdmin(admin.ModelAdmin):
-    list_display=['title','price','discount','category','description']
+    list_display=['title','price','discount','category', 'created_at']
     ordering = ['title','price']
