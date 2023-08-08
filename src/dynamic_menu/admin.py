476569ff4af4a1833f93a0d92a89def8d497b405
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-09 02:25:33 +0330

    feat: register dynamic menu in the admin panel

--- src/dynamic_menu/admin.py
+++ src/dynamic_menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MainInfo,Social
 
-# Register your models here.
+
+@admin.register(MainInfo)
+class MainInfoAdmin(admin.ModelAdmin):
+    exclude = ("id",)
+    list_display = ('title',)
+
+
+@admin.register(Social)
+class SocialAdmin(admin.ModelAdmin):
+    exclude = ("id",)
