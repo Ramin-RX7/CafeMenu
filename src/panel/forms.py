f3b01ff04722aef74acc9bc892f773274355e264
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-09 00:43:22 +0330

    Feat:add EditOrderItemForm, EditOrderForm to panel forms.py

---
+++ src/panel/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.core.validators import RegexValidator
+from users.models import PhoneNumberField
+from orders.models import OrderItem,Order
+from django.forms import ModelForm
+
+
+
+
+
+
+class EditOrderItemForm(ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = ['food', 'quantity', 'unit_price', 'discount']
+
+
+class EditOrderForm(ModelForm): 
+    class Meta:
+        model = Order
+        fields = "__all__"
