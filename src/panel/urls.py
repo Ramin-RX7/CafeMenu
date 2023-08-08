200fa6161d2abfcb788a06680e252917263a724e
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-08 15:44:11 +0330

    refactor: chang dashboard url links

--- src/panel/urls.py
+++ src/panel/urls.py
@@ -6,10 +6,10 @@ app_name = "panel"
 
 
 urlpatterns = [
-    path("",dashboard_staff, name="dashboard_staff"),
-    path("order/edit/<int:order_id>/", edit_order, name="edit_order"),
-    path("order/approve/<int:order_id>/", approve_order, name="approve_order"),
-    path("order/reject/<int:order_id>/", reject_order, name="reject_order"),
-    path("order/pay/<int:order_id>/", pay_order, name="pay_order"),
-    path("order/deliver/<int:order_id>/", deliver_order, name="deliver_order"),
+    path("",dashboard_staff, name="dashboard"),
+    path("order/<int:order_id>/edit/", edit_order, name="edit_order"),
+    path("order/<int:order_id>/approve/", approve_order, name="approve_order"),
+    path("order/<int:order_id>/reject/", reject_order, name="reject_order"),
+    path("order/<int:order_id>/pay/", pay_order, name="pay_order"),
+    path("order/<int:order_id>/deliver/", deliver_order, name="deliver_order"),
 ]
