0215209eca36e47cfac60e1836a415536188132c
sajjadp96 <sajjad.piri1997@gmail.com>
2023-08-08 08:25:11 +0330

    panel:views count of order for day week and month defined

--- src/panel/views.py
+++ src/panel/views.py
@@ -10,7 +10,10 @@ def dashboard(request):
     last_7_days = datetime.datetime.today() - datetime.timedelta(7)
     last_30_days = datetime.datetime.today() - datetime.timedelta(30)
     
-    # price for each hour of day
+    #-----------------------------------------------------------------------------------
+    
+    # price of sales part
+    
     price_for_each_hour_of_last_day = {} 
     for i in range(0,last_days.hour):
         hour = f'{i}'
@@ -36,3 +39,33 @@ def dashboard(request):
         price_for_each_day_of_month[k['day']]=k['price']
     
     total_price = all_orders.filter(status="Paid").aaggregate(total=Sum('price'))
+
+    #----------------------------------------------------------------------------------------
+    
+    # count of order part
+    
+    count_order_for_each_hour_of_last_day = {} 
+    for i in range(7,last_days.hour):
+        hour = f'{i}'
+        data = all_orders.filter(Q(created_at__date=last_days) & Q(created_at__hour__gte=i) & Q(created_at__hour__lt=i+1))\
+        .values('price').annotate(count=Count('id'))
+        if data:
+            count_order_for_each_hour_of_last_day[hour]=data[0]['count']
+        else:
+            count_order_for_each_hour_of_last_day[hour]=0
+    
+    count_order_for_each_day_of_week ={}
+    price_week = all_orders.filter(created_at__gte=last_7_days).\
+    extra({'day':"date(created_at)"}).\
+    values('day').annotate(count=Count('id'))
+    for k in price_week:
+        count_order_for_each_day_of_week[k['day']]=k['count']
+        
+    count_order_for_each_day_of_month={}              
+    price_month = all_orders.filter(created_at__gte=last_30_days).\
+    extra({'day':"date(created_at)"}).\
+    values('day').annotate(count=Count('id'))
+    for k in price_month:
+        count_order_for_each_day_of_month[k['day']]=k['count']
+    
+    total_count = all_orders.aaggregate(count=Count('id'))
