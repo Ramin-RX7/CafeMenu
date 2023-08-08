5474fb831ac20f464d6fafdf299dbe01296f8b87
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-09 03:11:47 +0330

    feat: convert search to CBV

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -9,7 +9,7 @@ app_name = "foods"
 urlpatterns = [
     path("categories/", category_list, name="category_list"),
     path("category/<int:id>/", category_details, name="category_details"),
-    path("search/", search, name="search"),
+    path("search/", SearchView.as_view(), name="search"),
     path("<int:id>/", food_details, name="food_details"),
     path("menu/", menu, name="menu")
 ]
