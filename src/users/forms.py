793dd32f9db23451e1c5d0ae1b516e2214e98cd2
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-10 02:48:14 +0330

    feat: add UserChangeForm

--- src/users/forms.py
+++ src/users/forms.py
@@ -16,3 +16,20 @@ class UserAddForm(UserCreationForm):
         self.fields['last_name'].required = False
 
 
+class UserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('phone', 'first_name', 'last_name', 'is_staff', 'is_active')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = False
+        self.fields['last_name'].required = False
+
+    password = forms.CharField(widget=forms.PasswordInput(), required=False)
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if password:
+            self.instance.set_password(password)
+        return password
