647b89ff87ba38895d0356ad096c03662becb8d7
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-15 12:04:46 -0700

    fix: edit url for category detail

--- src/foods/urls.py
+++ src/foods/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("categories/", CategoryListView.as_view(), name="category_list"),
     path("category/<int:id>/", CategoryDetailView.as_view(), name="category_details"),
     path("search/", SearchView.as_view(), name="search"),
-    path("<int:id>/", food_details, name="food_details"),
+    path("<int:id>/", FoodDetailView.as_view(), name="food_details"),
     path("menu/", menu, name="menu")
 ]
