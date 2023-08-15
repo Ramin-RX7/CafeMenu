0affe26d5beb45f3de1836264ab01e3972a090cb
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-16 01:41:30 +0330

    fix: extra code in index view

--- src/main/views.py
+++ src/main/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+
 class IndexTemplateView(TemplateView):
     template_name = "index.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-    
+
 class AboutUsTemplateView(TemplateView):
     template_name = "main/about_us.html"
 
