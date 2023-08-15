f8d0839d946df111f099eaa08281e096b62d839e
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-16 01:55:55 +0330

    fix: FoodDetailView extra code

--- src/foods/views.py
+++ src/foods/views.py
@@ -19,7 +19,7 @@ class CategoryDetailView(DetailView):
     model = Category
     template_name = 'foods/category_details.html'
     context_object_name = 'category'
-    pk_url_kwarg = 'id'  
+    pk_url_kwarg = 'id'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -27,16 +27,12 @@ class CategoryDetailView(DetailView):
         context['foods'] = category.food_set.all()
         return context
 
+
 class FoodDetailView(DetailView):
     model=Food
     template_name="foods/food_details.html"
     pk_url_kwarg='id'
     context_object_name='food'
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context= super().get_context_data(**kwargs)
-        food=self.get_object()
-        context['food']=food
-        return context
 
 
 class SearchView(View):
