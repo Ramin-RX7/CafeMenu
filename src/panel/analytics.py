4d474676dcb4f01d86bf404ec2792410ed4e1b52
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-15 15:19:30 +0330

    feat: add unique_customers_rel query

--- src/panel/analytics.py
+++ src/panel/analytics.py
@@ -235,3 +235,17 @@ def customerSales_rel(days):
     return {"old":top_customers}
 
 
+
+def unique_customers_rel(days):
+    unique_customers_by_day = defaultdict(set)
+
+    for day in range(days):
+        end_date =   datetime.now() - timedelta(days=day)
+        start_date = datetime(end_date.year, end_date.month, end_date.day, 0, 0, 0)
+        end_date =   datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
+
+        unique_customers = ALL_ORDERS.filter(created_at__range=(start_date, end_date)).values('customer').distinct()
+
+        unique_customers_by_day[day] = len(unique_customers)
+
+    return list(unique_customers_by_day.values())[::-1]
