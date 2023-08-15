a371200e145467283278778dba523c4141102add
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-15 10:35:44 -0700

    feat: IndexTemplateView added

--- src/main/views.py
+++ src/main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def index(request):
-    context = {}
-    return render(request, "index.html", context)
+class IndexTemplateView(TemplateView):
+    template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 def about_us(request):
     context = {}
