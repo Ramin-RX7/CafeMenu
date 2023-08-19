7d4313ca7d75aacb05184893b9655af71ceef909
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-19 20:26:38 +0330

    feat: add orderitems dataset download

--- src/panel/views_manager.py
+++ src/panel/views_manager.py
@@ -72,6 +72,22 @@ def json_api(request):
     return HttpResponse(json.dumps(context))
 
 
+def export_order_items_to_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="order_items.csv"'
+
+    order_items = OrderItem.objects.all()
+
+    writer = csv.writer(response)
+    writer.writerow(['Order ID', 'Customer', 'Food', 'Quantity', 'Unit Price', 'Discount'])
+
+    for order_item in order_items:
+        writer.writerow([order_item.order.id, order_item.order.customer, order_item.food.title, order_item.quantity,
+                         order_item.unit_price, order_item.discount])
+
+    return response
+
+
 
 def analytics(request):
     context = {
