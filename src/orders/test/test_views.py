e4480e969725e391735bef08c2086097bccc8284
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-20 01:07:58 +0330

    feat:add test_index_GET for IndexView class

--- src/orders/test/test_views.py
+++ src/orders/test/test_views.py
@@ -8,4 +8,8 @@ class TestViews(TestCase):
         self.client = Client()
         self.order_list_url = reverse('orders:order_list')
         self.index = reverse('orders:index')
+        
+    def test_index_GET(self):
+        response = self.client.get(self.order_list_url)
+        self.assertEquals(response.status_code, 302)
     
