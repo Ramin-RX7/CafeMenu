0ef5e0a3951483af9c605cf44b9127999f76fd45
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-19 12:11:21 -0700

    feat: core view tests for index, about us

--- src/core/test/test_views.py
+++ src/core/test/test_views.py
@@ -1,4 +1,3 @@
-from ..views import IndexTemplateView,AboutUsTemplateView
 from django.test import TestCase
 from django.urls import reverse
 
@@ -22,4 +21,22 @@ class IndexTemplateViewTest(TestCase):
         self.assertContains(response,'<h1 class="subtitle">Welcome To Our Restaurant</h1>')
         self.assertNotContains(response,'Not on the page')
 
-    
+
+class AboutUsTemplateViewTest(TestCase):
+
+    def test_url_exist(self):
+        response=self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
+
+    def test_url_available_by_name(self):
+        response=self.client.get(reverse("about_us"))
+        self.assertEqual(response.status_code,200)
+
+    def test_template_name_correct(self):
+        response=self.client.get(reverse('about_us'))
+        self.assertTemplateUsed(response,'core/about_us.html')
+
+    def test_template_content(self):
+        response=self.client.get(reverse('about_us'))
+        self.assertContains(response,'<h1 class="my-5">About CafeAmoo</h1>')
+        self.assertNotContains(response,'Not on the page')
