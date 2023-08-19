708d3bc8594b157322c531d48a4774d73a38259e
sajjadp96 <sajjad.piri1997@gmail.com>
2023-08-19 13:20:05 +0330

    foods:test models done

---
+++ src/foods/test/test_models.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from foods.models import Category,Food
+
+
+
+class ModelFoodTest(TestCase):
+    
+    def setUp(self):
+        self.category = Category.objects.create(
+            title = "category test",
+            description ="Category descriptoin test"
+        )
+        
+        self.food = Food.objects.create(
+            title = "test food",
+            description ="food descriptoin test",
+            price = 10,
+            category = self.category,
+            is_active =False
+        )
+        
+    def test_category_model(self):
+        self.assertEqual(f"{self.category.title}","category test")
+        self.assertEqual(f"{self.category.description}","Category descriptoin test")
+    
+    def test_food_model(self):
+        self.assertEqual(f"{self.food.title}","test food")
+        self.assertEqual(f"{self.food.description}","food descriptoin test")
+        self.assertEqual(f"{self.food.price}",'10')
+        self.assertEqual(f"{self.food.category}","category test")
+        self.assertEqual(f"{self.food.is_active}",'False')
+        
+    
