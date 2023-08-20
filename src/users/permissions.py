07f465ca33c248011709ef90b417c97450073be8
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-20 15:04:58 +0330

    feat: add category and table to staff permissions

--- src/users/permissions.py
+++ src/users/permissions.py
@@ -1,15 +1,19 @@
 from django.contrib.auth.models import Group,Permission
 
 
+
+#> Normal staff
 normal_staff, created = Group.objects.get_or_create(name='Normal Staff')
 
-permission_codenames = [
+staff_permissions = [
     'view_user',
     'view_order',
     'change_order',
     'view_food',
     'view_maininfo',
     'view_socials',
+    'view_category',
+    'view_table',
 ]
 
 permissions = Permission.objects.filter(codename__in=permission_codenames)
