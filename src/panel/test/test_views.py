a6c11508f27598545911bda41e0527db3022f9a2
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-22 20:28:03 +0330

    test: create testviews and test login func

---
+++ src/panel/test/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase,Client
+from django.urls import reverse
+import json
+
+
+class TestViews(TestCase):
+
+    def test_login_GET(self):
+        self.url=reverse("panel:login")
+        response = self.client.get(self.url)
+
+        self.assertEquals(response.status_code,200)
+
