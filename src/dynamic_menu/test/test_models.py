37a6b18f948430f7cba47a7a15c1eac9a0c1ae6e
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-22 09:56:20 -0700

    chore: Singletone model test edited

--- src/dynamic_menu/test/test_models.py
+++ src/dynamic_menu/test/test_models.py
@@ -1,94 +1,25 @@
 from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.exceptions import ValidationError
 from ..models import MainInfo, Social
 
-class MainInfoModelTest(TestCase):
-
-    def setUp(self):
-        self.logo = SimpleUploadedFile("logo.png", b"file_content", content_type="image/png")
-        self.icon = SimpleUploadedFile("icon.png", b"file_content", content_type="image/png")
-
-        self.main_info = MainInfo.objects.create(
-            title="Test Title",
-            motto="Test Motto",
-            short_description="Test Short Description",
-            about_us="Test About Us",
-            phone="1234567890",
-            email="test@example.com",
-            logo=self.logo,
-            icon=self.icon,
-        )
-
-    def test_main_info_str(self):
-        self.assertEqual(str(self.main_info), "Main Info")
-
-    def test_main_info_singleton(self):
-        main_info_1 = MainInfo.objects.create(
-            title="Test Title",
-        )
-        main_info_2 = MainInfo.objects.create(
-            title="Another Title",
-        )
-        self.assertEqual(main_info_1, main_info_2)
-
-
-class SocialModelTest(TestCase):
-
-    def setUp(self):
-        self.social = Social.objects.create(
-            instagram="https://www.instagram.com/test/",
-            telegram="https://t.me/test/",
-            whatsapp="https://whatsapp.com/test/",
-            youtube="https://www.youtube.com/test/",
-            facebook="https://www.facebook.com/test/",
-            tweeter="https://twitter.com/test/",
-        )
-
-    def test_social_str(self):
-        self.assertEqual(str(self.social), "Social")
+class SingletonModelTestCase(TestCase):
+    
+    def test_maininfo_singleton(self):
+        MainInfo.objects.create(title="Test", phone="1234567890", logo="path/to/logo.jpg", icon="path/to/icon.jpg")
+        self.assertEqual(MainInfo.objects.count(), 1)        
+        with self.assertRaises(ValidationError):
+            MainInfo.objects.create(title="Test2", phone="0987654321", logo="path/to/logo2.jpg", icon="path/to/icon2.jpg")
+        self.assertEqual(MainInfo.objects.count(), 1)        
+        instance = MainInfo.objects.first()
+        instance.delete()
+        self.assertEqual(MainInfo.objects.count(), 1)
 
     def test_social_singleton(self):
-        social_1 = Social.objects.create(
-            instagram="https://www.instagram.com/test/",
-        )
-        social_2 = Social.objects.create(
-            instagram="https://www.instagram.com/another/",
-        )
-
-        self.assertEqual(social_1, social_2)
-
-from django.test import TestCase
-from dynamic_menu.models import MainInfo,Social
-
-class MainInfoModelTest(TestCase):
-    def test_create_instance(self):
-        main_info = MainInfo.objects.create(
-            title="Test Title",
-            motto="Test Motto",
-            short_description="Test Description",
-            about_us="Test About Us",
-            phone="1234567890",
-            email="test@email.com",
-            logo="images/dynamic_menu/test_logo.png",
-            icon="images/dynamic_menu/test_icon.png"
-        )
-        self.assertEqual(str(main_info), "Main Info")
-
-    def test_str_method(self):
-        main_info = MainInfo.objects.create(title="Test Title")
-        self.assertEqual(str(main_info), "Main Info")
-
-
-class SocialModelTest(TestCase):
-    def test_create_instance(self):
-        social = Social.objects.create(
-            instagram="test_instagram",
-            telegram="test_telegram",
-            whatsapp="test_whatsapp",
-            youtube="test_youtube",
-            facebook="test_facebook",
-            tweeter="test_tweeter"
-        )
-        self.assertEqual(social.instagram, "test_instagram")
-        self.assertEqual(social.telegram, "test_telegram")
-
+        Social.objects.create()
+        self.assertEqual(Social.objects.count(), 1)        
+        with self.assertRaises(ValidationError):
+            Social.objects.create()
+        self.assertEqual(Social.objects.count(), 1)        
+        instance = Social.objects.first()
+        instance.delete()
+        self.assertEqual(Social.objects.count(), 1)
