7de3d993378c275673229c6270e9001e398a125a
sajjadp96 <sajjad.piri1997@gmail.com>
2023-08-22 15:42:40 +0330

    foods:test models str test done

--- src/foods/test/test_models.py
+++ src/foods/test/test_models.py
@@ -23,6 +23,9 @@ class ModelFoodTest(TestCase):
         self.assertEqual(f"{self.category.title}","category test")
         self.assertEqual(f"{self.category.description}","Category descriptoin test")
     
+    def test_str_category(self):
+        self.assertEqual(str(self.category),"category test")
+        
     def test_food_model(self):
         self.assertEqual(f"{self.food.title}","test food")
         self.assertEqual(f"{self.food.description}","food descriptoin test")
@@ -30,4 +33,8 @@ class ModelFoodTest(TestCase):
         self.assertEqual(f"{self.food.category}","category test")
         self.assertEqual(f"{self.food.is_active}",'False')
         
+    def test_str_food(self):
+        self.assertEqual(str(self.food),"test food")
+        
+            
     
