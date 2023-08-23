882bee66ac93ccc99811ec1ce7fd777d2e73820b
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-23 12:53:49 +0330

    test:add test_lookups_order_detail_filter function

---
+++ src/orders/test/test_admin.py
@@ -0,0 +1,24 @@
+from django.test import TestCase, RequestFactory
+from orders.admin import OrderToDayFilter, OrderStatusFilter, OrderAdmin, TableAdmin
+from orders.models import Order
+from unittest.mock import Mock
+from django.contrib.admin import SimpleListFilter
+from datetime import datetime, timedelta
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin import ModelAdmin
+from orders.models import Table, Order, OrderItem
+from users.models import User 
+
+def test_lookups_order_detail_filter(self):
+        filter_instance = OrderToDayFilter(
+            request=None,
+            params={},
+            model=Order,
+            model_admin=None
+        )
+        
+        expected_lookups = [('today', 'today')]
+        self.assertEqual(filter_instance.lookups(None, None), expected_lookups)
+
+
+
