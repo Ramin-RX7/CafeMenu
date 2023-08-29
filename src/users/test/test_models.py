887bfc653d3ddc924bffeacd56e2d7dec823bfb9
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-29 16:36:54 -0700

    test: phone number field tests

--- src/users/test/test_models.py
+++ src/users/test/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from users.models import User
+from core.validators import phone_validator
 
 
 class UserManagerTestCase(TestCase):
@@ -38,5 +39,31 @@ class UserManagerTestCase(TestCase):
             User.objects.create_superuser(phone='9123456789',
                                           password='password',
                                           is_superuser=False)
+
+
+
+class PhoneNumberFieldTestCase(TestCase):
+
+    def test_get_prep_wrong_value(self):
+        value='011122345678'
+        with self.assertRaises(ValidationError):
+            regex = phone_validator(value)
+
+    def test_get_prep_value(self):
+        value='9123456789'
+        regex = phone_validator(value)
+        phone_parts = regex.groupdict()
+        self.assertEqual(phone_parts["operator"],'912')
+        self.assertEqual(phone_parts['middle3'],'345')
+        self.assertTrue(phone_parts['last4'],'6789')
+
+
+    def test_get_phone(self):
+        value='00989123456789'
+        regex = phone_validator(value)
+        phone_parts = regex.groupdict()
+        phone = phone_parts["operator"]+phone_parts["middle3"]+phone_parts["last4"]
+        self.assertEqual(phone,'9123456789')
+
 
 
