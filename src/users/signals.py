ede9f21ce8b8dfe47692c91b97a0ac054001b752
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-31 00:34:57 +0330

    fix: remove signal file

--- src/users/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
-from django.apps import apps
-
-@receiver(post_migrate)
-def apply_permissions(sender, **kwargs):
-    if sender.label == apps.get_app_config('users'):
-        from . import permissions
