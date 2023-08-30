53e512fd2d18a0d7b925e0d605b3a4f92ea7b339
hosseink9 <hosseinkalantari.29@gmail.com>
2023-08-30 13:03:43 +0330

    test: create a edit order class and some models

--- src/panel/test/test_views.py
+++ src/panel/test/test_views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from panel.forms import *
 import json
 from users.models import User
+from orders.models import Table,Order,OrderItem
+from foods.models import Food,Category
 
 class TestViews(TestCase):
 
@@ -58,3 +60,51 @@ class TestLoginView(TestCase):
         response = self.client.get(self.url)
 
         self.assertTemplateUsed(response,'panel/dashboard_staff.html')
+
+
+
+
+class TestEditOrder(TestCase):
+    def setUp(self):
+        User.objects.create_superuser(phone='09123456789',password='1234')
+        self.client= Client()
+        self.client.login(phone='09123456789',password='1234')
+
+        self.category= Category.objects.create(
+            title='Iranian Foods'
+        )
+
+        self.food = Food.objects.create(
+            title='Kabab',
+            price=10,
+            category=self.category
+        )
+
+        self.table = Table.objects.create(
+            name='table 1',
+            is_reserved=False,
+        )
+
+        self.user = User.objects.create(
+            phone="9176877108",
+            first_name = 'Ali',
+            last_name= 'Afzal',
+            is_active=True,
+            is_staff=False,
+        )
+
+        self.order = Order(
+            customer='9176877108',
+            table=self.table,
+            discount=2.5,
+            responsible_staff=self.user,
+        )
+        self.order.save(check_items=False)
+
+        self.order_item = OrderItem.objects.create(
+            order=self.order,
+            food=self.food,
+            unit_price=self.food.price,
+            quantity=4,
+            discount=0.0,
+            )
