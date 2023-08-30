0642c0e65f2a57840501f887d61da4ce1b56e505
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-30 15:00:37 +0330

    refactor: move cart price calculation function to cart.js

--- src/static/js/cart.js
+++ src/static/js/cart.js
@@ -66,3 +66,15 @@ document.getElementById('sendDataButton').addEventListener('click', function() {
     orderForm.submit();
 });
 
+
+function calculateCart(){
+    const elementsToSum = document.querySelectorAll('.item-total-price');
+    let sum = 0;
+    elementsToSum.forEach(element => {
+        sum += parseFloat(element.textContent);
+    });
+    const totalPrice = document.getElementById("total-price")
+    if (totalPrice){
+        totalPrice.textContent = sum.toString(10)+"$";
+    }
+}
