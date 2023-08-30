e5a5cc1cc18e5d0a0eb6387eee3c951348ab7f73
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-08-31 00:30:56 +0330

    fix:fix form_views.py

--- src/orders/test/test_forms.py
+++ src/orders/test/test_forms.py
@@ -1,19 +1,11 @@
 from django.test import SimpleTestCase
 from orders.forms import CustomerLoginForm, PhoneInput
 
-class TestForms(SimpleTestCase):
-    def test_customer_login_form_valid_data(self):
-        form = CustomerLoginForm(data={
-            'phone' : '9176877108',
-        })
-
-        self.assertTrue(form.is_valid())
-        
+class TestForms(SimpleTestCase):    
     def test_customer_login_form_valid_data(self):
         form = CustomerLoginForm(data={
             'phone' : '123456789',
         })
-        # print(form.errors)
         self.assertFalse(form.is_valid())
         
     def test_customer_login_form_no_data(self):
